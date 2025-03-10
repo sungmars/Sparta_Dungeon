848cc15102b274c1b1ba21011d35984b8b584dde
sungmars <136216542+sungmars@users.noreply.github.com>
2025-03-11 07:27:35 +0900

    ⚡️<Add>: “아이템 관련 스크립트 추가”

---
+++ Assets/Scripts/Object/ItemData.cs
@@ -0,0 +1,39 @@
+using System;
+using UnityEngine;
+
+public enum ItemType
+{
+    Equipable,
+    Consumable,
+    Resource
+}
+public enum ConsumableType
+{
+    Health
+}
+[Serializable]
+public class ItemDataConsumable
+{
+    public ConsumableType type;
+    public float value;
+}
+[CreateAssetMenu(fileName ="Item",menuName ="New Item")]
+public class ItemData : MonoBehaviour
+{
+    [Header("Info")]
+    public string displayName;
+    public string description;
+    public ItemType type;
+    public Sprite icon;
+    public GameObject dropPrefab;
+
+    [Header("Stacking")]
+    public bool canStack;
+    public int maxStackAmount;
+
+    [Header("Consumable")]
+    public ItemDataConsumable[] consumables;
+
+    [Header("Equip")]
+    public GameObject equipPrefab;
+}
